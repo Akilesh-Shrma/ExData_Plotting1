1f5ba2da6e551b44c3e46c552b6d74959e7b2c18
Akilesh-Shrma <36205720+Akilesh-Shrma@users.noreply.github.com>
2018-03-08 21:31:12 +0530

    Add files via upload

--- Plot4.R
+++ Plot4.R
@@ -27,14 +27,30 @@ sub_power$DateTime <- strptime(paste(sub_power$Date, sub_power$Time),
 sub_power$DateTime <- as.POSIXct(sub_power$DateTime)
 
 
-### Plot 3 - Energy Sub Metering vs Date
+### Plot 4 - 4 different plots in one device
+par(mfrow = c(2, 2))
+
+### Plot 1 - Top-left
+plot(sub_power$Global_active_power ~ sub_power$DateTime, type = "l", xlab = "", 
+     ylab = "Global Active Power (kilowatts)")
+
+### Plot 2 - Top Right
+plot(sub_power$Voltage ~ sub_power$DateTime, type = "l", xlab = "datetime", 
+     ylab = "Voltage")
+
+### Plot 3 - Bottom Left
 with(sub_power, {
         plot(Sub_metering_1 ~ DateTime, type = "l", xlab = "", 
              ylab = "Energy sub metering")
         lines(Sub_metering_2 ~ DateTime, col = "red")
         lines(Sub_metering_3 ~ DateTime, col = "blue")
-        legend("topright", col = c("black", "red", "blue"), lty = 1, 
+        legend("topright", col = c("black", "red", "blue"), 
                legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 })
-dev.copy(png, file = "plot3.png", height = 480, width = 480)
+
+### Plot 4 - Bottom Right
+plot(sub_power$Global_reactive_power ~ sub_power$DateTime, type = "l", xlab = "Global_reactive_power", 
+     ylab = "datetime")
+
+dev.copy(png, file = "plot4.png", height = 480, width = 480)
 dev.off()
